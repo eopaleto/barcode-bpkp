dfc519df03767ffbb101e7f50311569fc7ee3a8a
eopaleto <satryopangsestu51@gmail.com>
2025-08-23 17:07:54 +0700

    23 Agustus 2025 Ep 5

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('barcode')
+            ->path('')
             ->login()
             ->brandLogo(asset('img/logo-bpkp.png'))
             ->brandLogoHeight('50px')
